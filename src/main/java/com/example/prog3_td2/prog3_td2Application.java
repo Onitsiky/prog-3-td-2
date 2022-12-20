9dbcd52f5cf776744832c446b1b757049ab6c6ab
Onitsiky <hei.onitsiky@gmail.com>
2022-12-20 08:23:56 +0300

    chore : initialize project

---
+++ src/main/java/com/example/prog3_td2/prog3_td2Application.java
@@ -0,0 +1,13 @@
+package com.example.prog3_td2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class prog3_td2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(prog3_td2Application.class, args);
+  }
+
+}
